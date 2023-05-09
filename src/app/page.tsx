0c99888fea4b29936dfa681d5c87a09b6f60e1f5
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-09 12:04:28 -0300

    feat: add background to page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-        Get started by editing
-      </p>
-    </main>
-  )
-}
+const Home = () => (
+  <div
+    className={`bg-[url('/image/bitcoin-background.png')] bg-no-repeat bg-center bg-cover min-h-screen `}
+  >
+    <p className='text-white'>Crypto Website</p>
+  </div>
+)
+
+export default Home
