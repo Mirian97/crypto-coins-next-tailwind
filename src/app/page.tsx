55654b38d6d8d52fdd4064d9c67ccd7be9f6f9e0
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-09 17:42:10 -0300

    feat: add component cart to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import Card from '@/components/Card'
+
 const Home = () => (
   <div
-    className={`bg-[url('/image/bitcoin-background.png')] bg-no-repeat bg-center bg-cover min-h-screen `}
+    className={`bg-[url('/image/bitcoin-background.png')] bg-no-repeat bg-center bg-cover min-h-screen p-10`}
   >
-    <p className='text-white'>Crypto Website</p>
+    <div className='w-full flex flex-row items-center justify-center gap-5 flex-wrap'>
+      <Card
+        name='Bitcoin'
+        image='https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg'
+        symbol='btc'
+        currentValue={12398493}
+        currentPercentage={-0.24}
+      />
+    </div>
   </div>
 )
 
