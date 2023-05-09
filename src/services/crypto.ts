ab9785dd348874a6e6e7c46c11516f1c90b11668
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-09 19:16:55 -0300

    feat: created async function to get crypto coins

--- src/services/crypto.ts
+++ src/services/crypto.ts
@@ -1,6 +1,6 @@
 import api from './api'
 
-const getCryptoCoins = async () => {
+export const getCryptoCoins = async () => {
   const { data } = await api.get('/coins/markets?vs_currency=brl&locale=pt')
   return data
 }
