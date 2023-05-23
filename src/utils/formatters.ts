04e212a7cf437251954eacf304a3ad413a996c1e
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-23 16:06:32 -0300

    feat: function to format date created

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -2,3 +2,9 @@ export const formatNumberToMoney = (value: number) =>
   value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
 
 export const formatNumberToPercentage = (value: number) => `${value.toFixed(2)}%`
+
+export const formatDate = (date: string) => {
+  const dateParts = date.split('-')
+  const formattedDate = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`
+  return formattedDate
+}
