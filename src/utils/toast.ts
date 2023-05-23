f811382561414c22e39c099bf02d3c7add12bceb
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-23 16:04:51 -0300

    feat: toast error created

---
+++ src/utils/toast.ts
@@ -0,0 +1,19 @@
+import { toast } from 'react-toastify'
+
+export function messageError(messageContent: string) {
+  toast.error(messageContent, {
+    position: 'top-center',
+    autoClose: 2500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'colored',
+    style: {
+      fontSize: '1rem',
+      fontWeight: 'bold',
+      borderRadius: '10px'
+    }
+  })
+}
